88ae8c45efe5745fb7a94a790ffd8a73892668f9
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-08-24 16:15:17 -0700

    modified servers.js and added index.js on routes folder

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const { MongoClient } = require('mongodb');
-const routes = require("./routes")
+const { MongoClient } = require("mongodb");
+// const routes = require("./routes");
+const db = require("./config/connection")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use(routes);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use(routes);
 
 const connectionStringURI = `mongodb://localhost/`;
-const dbName = 'socialNetwork';
+const dbName = "socialNetwork";
+
+db.once("open", () =>{
+    app.listen(PORT, () => console.log(`Server connected to localhost:${PORT}`));
+})
+// mongoose.connect(process.env.MONGODB_URI || connectionStringURI + dbName, {
+//   useFindAndModify: false,
+//   useNewUrlParser: true,
+//   userUnifiedTopology: true,
+// });
+
+mongoose.set("debug", true);
 
 
-mongoose.connect(process.env.MONGODB_URI || connectionStringURI+dbName, {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    userUnifiedTopology: true
-});
-
-mongoose.set('debug', true)
-
-app.listen(PORT, ()=> console.log(`Server connected to localhost:${PORT}`));
